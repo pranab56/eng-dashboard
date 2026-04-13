242fc69dfa005ab908dd20216de847220c05496c
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-04-13 16:57:24 +0600

    Sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -32,36 +32,38 @@ const Sidebar = () => {
   }
 
   return (
-    <div className='flex flex-col h-full'>
+    <div className='h-screen flex flex-col'>
       <div className='w-full flex items-center justify-center py-4 cursor-pointer border-b border-gray-700'>
         {logo && <Image src={logo} width={1000} height={300} alt="ENG Logo" className='w-[140px] h-auto' />}
       </div>
-      <div className='flex-1 flex flex-col justify-between'>
-        <div className='flex-1 flex flex-col gap-1 py-1 px-2'>
-          {sidebarData?.map((item: TMenuItem) => {
-            const isItemActive = isActive(item?.label);
-            const Icon = item.icon;
+      <div className='flex-1 overflow-y-auto hide-scrollbar flex flex-col justify-between'>
+        <div className=''>
+          <div className='flex flex-col gap-1 py-1 px-2'>
+            {sidebarData?.map((item: TMenuItem) => {
+              const isItemActive = isActive(item?.label);
+              const Icon = item.icon;
 
-            return (
-              <Link
-                href={item?.label}
-                key={item.id}
-                className={`flex gap-2 items-center py-3 px-4 rounded-md transition-colors duration-300 cursor-pointer ${isItemActive
-                  ? "text-white bg-[#373737] border-l-4 border-[#EABB00]"
-                  : "text-white border-l-4 border-transparent"
-                  }`}
-              >
-                <Icon
-                  className={`w-6 h-6 ${isItemActive ? "text-[#EABB00]" : "text-gray-400"
+              return (
+                <Link
+                  href={item?.label}
+                  key={item.id}
+                  className={`flex gap-2 items-center py-3 px-4 rounded-md transition-colors duration-300 cursor-pointer ${isItemActive
+                    ? "text-white bg-[#373737] border-l-4 border-[#EABB00]"
+                    : "text-white border-l-4 border-transparent"
                     }`}
-                />
-                <span className={`${isItemActive ? "text-[#EABB00]" : "text-gray-400"
-                  }`}>{item.title}</span>
-              </Link>
-            );
-          })}
+                >
+                  <Icon
+                    className={`w-6 h-6 ${isItemActive ? "text-[#EABB00]" : "text-gray-400"
+                      }`}
+                  />
+                  <span className={`${isItemActive ? "text-[#EABB00]" : "text-gray-400"
+                    }`}>{item.title}</span>
+                </Link>
+              );
+            })}
+          </div>
         </div>
-        <div className='flex flex-col gap-1 px-2 pb-8 pt-2'>
+        <div className='sticky bottom-0 flex flex-col gap-1 px-2 py-4 bg-black'>
           <button
             onClick={handleLogout}
             className='flex gap-2 items-center py-2 px-4 bg-red-500 hover:bg-red-600 transition-colors duration-300 rounded-md cursor-pointer'
