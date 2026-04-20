c218c5e2148f75db827f3bb1a4b6aed3f1b4bc65
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-04-20 08:38:21 +0600

    modify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
     },
+  },
+  experimental: {
     serverActions: {
       bodySizeLimit: "50mb",
-      allowedDevOrigins: [
+      allowedOrigins: [
         "http://localhost:3000",
       ],
     },
