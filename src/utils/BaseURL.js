2ae29605c3b7c4086f28a1b37b3384fd66e16fa1
pranab <pronabhalder53@gmail.com>
2026-07-16 16:30:32 +0600

    all ok

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://10.10.26.208:5005";
-// export const baseURL = "http://31.97.117.41:5005";
+// export const baseURL = "http://10.10.26.208:5005";
+export const baseURL = "http://31.97.117.41:5005";
