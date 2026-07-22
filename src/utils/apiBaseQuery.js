fc76156bae6899208a2869ee43fcf98bed09358e
pranab56 <pronabhalder53@gmail.com>
2026-07-22 15:03:13 +0600

    all ok

--- src/utils/apiBaseQuery.js
+++ src/utils/apiBaseQuery.js
@@ -17,5 +17,5 @@ export const baseApi = createApi({
     },
   }),
   endpoints: () => ({}),
-  tagTypes: ["table", "match", "team", "reword", "profile", "news", "terms", "privacy", "league", "referee", "leagueTeam", "manager-team"],
+  tagTypes: ["table", "match", "team", "reword", "profile", "news", "terms", "privacy", "league", "referee", "leagueTeam", "manager-team" , "notification"],
 });
