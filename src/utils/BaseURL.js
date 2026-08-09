a4d58f89c000263ee982dfd52320d5ee8ce44c0a
rakiburrahman307 <rakiburrahman307@gmail.com>
2026-08-09 18:45:54 +0600

    feat: add base URL utility with production API endpoint

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://10.10.26.180:5005";
-// export const baseURL = "https://api.engsportsevents.co.uk";
+// export const baseURL = "http://10.10.26.180:5005";
+export const baseURL = "https://api.engsportsevents.co.uk";
